eb689007beabe5ca4bb51a841b4f3e04d07c8bc7
Khaviya Srre. M <khav@Khaviyas-MacBook-Pro.local>
2024-05-31 13:51:16 +0530

    first commit

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb+srv://sr0451:KWApdUcLRDs2Ch7s@cluster0.xrty1vv.mongodb.net/LoginFormPractice")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection  
